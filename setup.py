5e75608676fc3f73655bb4985cb12711ff60191a
Dan Foster <dan@zem.org.uk>
2021-05-30 10:43:42 +0100

    Initial project layout

---
+++ setup.py
@@ -0,0 +1,27 @@
+import os
+from setuptools import setup, find_packages
+
+
+setup(
+    name = "vcloud",
+    version = "0.0.1",
+    author = "Dan Foster",
+    author_email = "dan@zem.org.uk",
+    description = "libvirt based cloud-init command line",
+    license = "Apache License 2.0",
+    packages = find_packages(),
+    long_description="""libvirt based cloud-init command line""",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+    ],
+    install_requires=[
+        "click"
+    ],
+    entry_points = {
+        'console_scripts': [
+            'vcloud=vcloud.cli:main'
+        ],
+        
+    }
+)
